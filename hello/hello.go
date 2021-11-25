190e86ac467913258946f5b0578105249b4b01a8
Joao Paulo <joaopaulozangrande@gmail.com>
2021-11-25 18:44:39 -0300

    showing the menu

--- hello/hello.go
+++ hello/hello.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	nome := "João"
-	idade := 10
 	versao := 1.1
-	fmt.Println("Olá, sr.", nome, "sua idade é",idade,"anos.")
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavel comando é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
 }
