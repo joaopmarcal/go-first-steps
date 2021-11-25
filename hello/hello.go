56c767335316f75c5a403e5606ccd0c8f9f03aae
Joao Paulo <joaopaulozangrande@gmail.com>
2021-11-25 18:27:12 -0300

    type inference

--- hello/hello.go
+++ hello/hello.go
@@ -1,16 +1,11 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main() {
-	var nome = "João"
-	var idade = 10
-	var versao float32 = 1.1
+	nome := "João"
+	idade := 10
+	versao := 1.1
 	fmt.Println("Olá, sr.", nome, "sua idade é",idade,"anos.")
 	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
-	fmt.Println("O tipo da variavel idade é", reflect.TypeOf(idade))
-	fmt.Println("O tipo da variavel versao é", reflect.TypeOf(versao))
 }
