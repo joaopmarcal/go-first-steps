9f46cea243d101c0523bb6a3bd5733aa8468a751
Joao Paulo <joaopaulozangrande@gmail.com>
2021-12-30 11:03:33 -0300

    I finished the project

--- hello/hello.go
+++ hello/hello.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
-	"strconv"
 )
 
 const monitoramentos = 3
@@ -27,6 +28,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do Programa")
 			os.Exit(0)
@@ -130,4 +132,16 @@ func registraLog(site string, status bool){
 	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
 
 	arquivo.Close()
+}
+
+func imprimeLogs(){
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
